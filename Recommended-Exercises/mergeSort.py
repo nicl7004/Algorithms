2515a656ab4685c6ddcb5fdb06cf504baf195cc9
Nicholas Clement <Nicholas.clement@colorado.edu>
2016-08-29 15:09:52 -0600

    multiple while loop working single not working

--- Recommended-Exercises/mergeSort.py
+++ Recommended-Exercises/mergeSort.py
@@ -1,16 +1,23 @@
 
-
+#attempt to merge in a single while loop
 def merge(a,b):
     i = j = 0
     c = []
-    while len(c)<(len(a)+len(b)):
-        if (i) >= (len(a)-1) and (j) >= (len(b)-1):
-            return c
-        elif (i) >= (len(a)-1):
+    while len(c)<=(len(a)+len(b)):
+
+        if (i) >= (len(a)):
             c.append(b[j])
+            if (i) > (len(a)-1) and (j) > (len(b)-1):
+                print("return")
+                print(i,"i",j,"j")
+            return c
             j+=1
-        elif (j) >= (len(b)-1):
+        elif (j) >= (len(b)):
             c.append(a[i])
+            if (i) > (len(a) - 1) and (j) > (len(b) - 1):
+                print("return")
+                print(i, "i", j, "j")
+                return c
             i+=1
         print("i =", i, "j =", j)
         if a[i]<b[j]:
@@ -22,10 +29,31 @@ def merge(a,b):
             j+= 1
             print("b[j]<a[i]")
 
-
+        print('all the way')
     return c
 
 a = [1,3,5,7]
-b = [2,4,6,8]
+b = [2,4,6,8,10,12]
 
 print(merge(a,b))
+
+def mergeBetter(a,b):
+    i = j = 0
+    c = []
+
+    while i < len(a) and j < len(b):
+        if a[i]>b[j]:
+            c.append(b[j])
+            j+=1
+        else:
+            c.append(a[i])
+            i+=1
+    while i<len(a):
+        c.append(a[i])
+        i+=1
+    while j < len(b):
+        c.append(b[j])
+        j+=1
+    return c
+
+print (mergeBetter(a,b))
