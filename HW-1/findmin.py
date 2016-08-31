d6d4b8e1a6976d129b2e21f0149864363943b952
Nick <nicholas.clement@colorado.edu>
2016-08-31 14:05:02 -0600

    working find min and array

--- HW-1/findmin.py
+++ HW-1/findmin.py
@@ -1,13 +1,26 @@
 
-a = [12,17,300,5,13,2,80,1]
+a = [12,17,300,5,13]
 x = 3
 def findMin(a,x):
     i = 0
     min = a[i]
-    while i < (x):
+    while i <=(x-1):
         if a[i]<min:
             min = a[i]
         i+=1
+
     return min
 
 print(findMin(a,x))
+
+
+def makeArray(a,x):
+    each = 1
+    b = []
+    while each <= x:
+        b.append(findMin(a,each))
+
+        each += 1
+    return b
+y = makeArray(a,x)
+print(y)
