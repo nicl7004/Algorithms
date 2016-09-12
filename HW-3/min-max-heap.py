b8c4f68bc515d472b471f1f90393ba306af1f5cc
Nick <Nicholas.clement@colorado.edu>
2016-09-12 14:39:23 -0600

    assignment 3

---
+++ HW-3/min-max-heap.py
@@ -0,0 +1,17 @@
+from random import randint
+x = [0] * 100
+y = [0] * 1000
+i = 0
+while i < len(x):
+    x[i] = randint(0,100)
+    i+=1
+# print (x)
+xMax = max(x)
+print (xMax)
+
+def bubbleUp(A, i)
+    if (i == 1):
+        return 0
+    else:
+        p = [i/2]
+    if (A[p]>A[i])
