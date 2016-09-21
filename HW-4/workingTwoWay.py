0417487e7ab730a8cdf798a9efe58d248e6a8c0f
Nick <Nicholas.clement@colorado.edu>
2016-09-21 17:38:04 -0600

    update

--- HW-4/workingTwoWay.py
+++ HW-4/workingTwoWay.py
@@ -6,28 +6,24 @@ def swap (a,i,j):
 def lomutoThreeWayPartition (a):
     n = len (a)
     pivot = a[n -1]
-    # print(pivot)
     i = -1 # todo : modify
     j =0 # todo : modify
     for j in range (0 ,n -1) :
         if (a [j] < pivot ):
-            i += 1
-            swap(a,i,j)
-        elif(a[j] == pivot):
             swap(a,i+1,j)
-    # print("Pivot", n-1, "i+1", i+1)
+            i += 1
+
     swap(a,i+1,n-1)
 
     return (a, "Pivot x=", pivot)
- # todo : fill in the code for this part ..
-        # elif (a[j] == pivot ):
- # todo : fill in the code for this part ..
- # todo : move the pivot element back to the correct place .
- # ...
 
 def test(a):
-    print("Orig array\n", a, "\n\n")
-    print("Partitioned array\n",lomutoThreeWayPartition(a), "\n\n")
+    print()
+    print("Orig array\n", a, "n")
+    print("Partitioned array\n",lomutoThreeWayPartition(a), "\n")
+
+
+
 
 arrayLength = int(input("Enter the length of the array you want to create."))
 array = [0]*arrayLength
@@ -35,4 +31,5 @@ y = 0
 while y < arrayLength:
     array[y] = randint(1,100)
     y+=1
+array[arrayLength-1] = 94
 test(array)
