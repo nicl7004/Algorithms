19dd6349c6b3f4a848cafe92c15ac903686842e5
Nick <Nicholas.clement@colorado.edu>
2016-09-21 15:50:25 -0600

    update of threeway

--- HW-4/lomutoThreeWay.py
+++ HW-4/lomutoThreeWay.py
@@ -9,12 +9,15 @@ def lomutoThreeWayPartition (a):
     # print(pivot)
     i = -1 # todo : modify
     j =0 # todo : modify
+    k = -1
     for j in range (0 ,n -1) :
         if (a [j] < pivot ):
             i += 1
+            k += 1
             swap(a,i,j)
         elif(a[j] == pivot):
-            swap(a,i+1,j)
+            k+=1
+            swap(a,k,j)
     # print("Pivot", n-1, "i+1", i+1)
     swap(a,i+1,n-1)
 
@@ -29,10 +32,11 @@ def test(a):
     print("Orig array\n", a, "\n\n")
     print("Partitioned array\n",lomutoThreeWayPartition(a), "\n\n")
 
-arrayLength = int(input("Enter the length of the array you want to create."))
-array = [0]*arrayLength
-y = 0
-while y < arrayLength:
-    array[y] = randint(1,100)
-    y+=1
+# arrayLength = int(input("Enter the length of the array you want to create."))
+# array = [0]*arrayLength
+# y = 0
+# while y < arrayLength:
+#     array[y] = randint(1,100)
+#     y+=1
+array = [20, 12, 17, 18, 9, 5, 4, 12, 8, 12]
 test(array)
