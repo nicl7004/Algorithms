be9c9624f5379f7fc3957e93ace4282021d31fcd
Nick <Nicholas.clement@colorado.edu>
2016-10-30 00:07:05 -0600

    working on add words

--- trieAssignmentKit/trieClass.py
+++ trieAssignmentKit/trieClass.py
@@ -8,8 +8,8 @@ import sys
 
 # We will use a class called my trie node
 class MyTrieNode:
-    # Initialize some fields 
-  
+    # Initialize some fields
+
     def __init__(self, isRootNode):
         #The initialization below is just a suggestion.
         #Change it as you will.
@@ -24,9 +24,10 @@ class MyTrieNode:
     def addWord(self,w):
         assert(len(w) > 0)
 
-        # YOUR CODE HERE
-        # If you want to create helper/auxiliary functions, please do so.
-        
+        for each in w:
+            self.next = each
+    
+
         return
 
     def lookupWord(self,w):
@@ -34,9 +35,9 @@ class MyTrieNode:
         # returns a number for the frequency and 0 if the word w does not occur.
 
         # YOUR CODE HERE
-        
+
         return 0 # TODO: change this line, please
-    
+
 
     def autoComplete(self,w):
         #Returns possible list of autocompletions of the word w
@@ -44,11 +45,11 @@ class MyTrieNode:
         #         word s occurs with frequency j
 
         #YOUR CODE HERE
-        
+
         return [('Walter',1),('Mitty',2),('Went',3),('To',4),('Greenland',2)] #TODO: change this line, please
-    
-    
-            
+
+
+
 
 if (__name__ == '__main__'):
     t= MyTrieNode(True)
@@ -63,11 +64,7 @@ if (__name__ == '__main__'):
     lst3 = t.autoComplete('pi')
     print('Completions for \"pi\" are : ')
     print(lst3)
-    
+
     lst4 = t.autoComplete('tes')
     print('Completions for \"tes\" are : ')
     print(lst4)
- 
-    
-    
-     
